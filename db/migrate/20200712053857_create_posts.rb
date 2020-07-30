42b963bb7bbc6d9c291351f79a6f0331bded0f3f
Hadakapantsu <sakuratsuduki@gmail.com>
2020-07-30 10:51:13 +0900

    first commit

---
+++ db/migrate/20200712053857_create_posts.rb
@@ -0,0 +1,8 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.text :content  #t.に続くのがカラムの型  :その右側にシンボルで記載されるものがカラム名
+      t.timestamps 
+    end
+  end
+end
